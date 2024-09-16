79964d358d8f66bc80daf8aeee3ee814cedfa685
nananghidayatulloh <nananghidayatulloh@gmail.com>
2024-09-16 10:33:09 +0700

    homepage ui

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,5 +1,8 @@
 import { formatAmount, formatAmountRupiah } from '@/lib/utils'
 import React from 'react'
+//import CountUp from 'react-countup'
+import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -8,6 +11,7 @@ const TotalBalanceBox = ({
     <section className='total-balance'>
         <div className='total-balance-chart'>
             {/* DoughnutChart */}
+            <DoughnutChart accounts={accounts} />
         </div>
 
         <div className='flex flex-col gap-6'>
@@ -20,9 +24,10 @@ const TotalBalanceBox = ({
                     Total Current Balance
                 </p>
 
-                <p className='total-balance-amount flex-center gap-2'>
-                    {formatAmountRupiah(totalCurrentBalance)}
-                </p>
+                <div className='total-balance-amount flex-center gap-2'>
+                    <AnimatedCounter amount={totalCurrentBalance} />
+                   {/*  {formatAmountRupiah(totalCurrentBalance)} */}
+                </div>
             </div>
         </div>
     </section>
